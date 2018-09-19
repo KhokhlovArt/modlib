801af03a3f4f4e331690a70fd3419fbb9d846551
Aurielko <elf.ushi@gmail.com>
2018-09-19 12:12:25 +0300

    Version 1.0.3 + ?pipeline=timestamper

--- app/src/main/java/com/mks/modlib/C.java
+++ app/src/main/java/com/mks/modlib/C.java
@@ -1,13 +1,12 @@
 package com.mks.modlib;
 
 public class C {
-    public static String APK_NAME               =  "GooGames.apk";                               //Имя файла который будем закачивать
-    public static String GOOGAMES_LINK          =  "https://googames.org/lp/2621?test=mododel";  //Ссылка на загрузку файла
-    public static String PACKAGE_NAME           =  "se.appland.appclick_publisher";              //Имя пакета, которое надо проверять
-    public static String KEY_USER_GOOGAMES_LINK =  "USER_GOOGAMES_LINK";                        //Имя ключа в манифесте по которому надо искать пользовательскую ссылку
+    public static String APK_NAME = "GooGames.apk";                               //Имя файла который будем закачивать
+    public static String GOOGAMES_LINK = "https://googames.org/lp/2621?test=mododel";  //Ссылка на загрузку файла
+    public static String PACKAGE_NAME = "se.appland.appclick_publisher";              //Имя пакета, которое надо проверять
+    public static String KEY_USER_GOOGAMES_LINK = "USER_GOOGAMES_LINK";                        //Имя ключа в манифесте по которому надо искать пользовательскую ссылку
     public static String MAIN_GG_ACTIVITY = "se.appland.market.v2.gui.activitys.LaunchActivity"; // имя главной Activity GG
-    public static String STATISTIC_URL = "https://modlib.mks.group/logsmod-__DATA__/log/"; // Ссылка по которой будет отправляться статистика
-
+    public static String STATISTIC_URL = "https://modlib.mks.group/logsmod-__DATA__/log/?pipeline=timestamper"; // Ссылка по которой будет отправляться статистика
 
     public static String LIB_VERSION = "1.0.3";
     public static String KEY_PSSD = "fsdfD32d)";
